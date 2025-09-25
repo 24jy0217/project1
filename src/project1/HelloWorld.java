f89c3d3bead9d4dda8db4de7a90d7c05364410f4
24jy0217 <24jy0217@PC01.jynet2.jec.ac.jp>
2025-09-25 12:26:38 +0900

    日本電子専門学校を10回表示する

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("日本電子専門学校");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("日本電子専門学校");			
+		}
 	}
 
 }
