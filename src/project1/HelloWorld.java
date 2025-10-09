4dc6675e2eb7fd0957c0dab356852f0a47a63027
24jy0217 Taniguchi Kanaho <24jy0217@jec.ac.jp>
2025-10-09 12:06:57 +0900

    HelloWorld.javaを整えた

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -3,9 +3,8 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		for (int i = 0; i < 10; i++) {
-			System.out.println("日本電子専門学校");			
+			System.out.println("日本電子専門学校");
 		}
 	}
 
